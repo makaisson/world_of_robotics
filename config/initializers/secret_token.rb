ed2804591986cbc26b9070059767059c251f8e64
Jared Shay <jared.shay@gmail.com>
2013-05-04 12:42:43 +1000

    Hide secret token.
    Read the secret token into the initializer from an uncommited file.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WorldOfRobotics::Application.config.secret_token = 'b47f16cb69ff16bdb9869dcdcb9907abf42b09f977806a5ae0893c876ccfd59cd93f9b2ffdbb96982e6376c18f714ccace7af61f7cd7ade1560a5ea886aa7a4a'
+WorldOfRobotics::Application.config.secret_token = File.read("token/secret-token")
