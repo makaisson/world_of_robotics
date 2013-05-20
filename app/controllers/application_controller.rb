85fab85bed5fc01f6f2bb187c49292d941d4e88e
Jared Shay <jared.shay@gmail.com>
2013-05-20 18:07:09 +1000

    add current_cart method with pending specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_cart
+    if session[:cart_id]
+      Cart.find(session[:cart_id])
+    else
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+  end
 end
