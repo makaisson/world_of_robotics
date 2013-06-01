7688a856d0f66ec24b16d0676e83f6a472615754
Jared Shay <jared.shay@gmail.com>
2013-06-01 11:02:22 +1000

    add new and create for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,18 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.find(params[:id])
+
+    if @category.save
+      redirect_to admin_index_path
+    else
+      render :new
+    end
+  end
 end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -17,4 +17,34 @@ describe CategoriesController do
     it { should respond_with(:success) }
     it { should render_template(:show) }
   end
+
+  describe "#new" do
+    before { get(:new) }
+
+    it { should render_template(:new) }
+  end
+
+  describe "#create" do
+    let(:category) { double(:category, id: 1) }
+    before do
+      Category.stub(:find) { category }
+    end
+    context "successfully" do
+      before do
+        category.should_receive(:save) { true }
+        post :create, id: category.id
+      end
+
+      it { should redirect_to(admin_index_path) }
+    end
+
+    context "unsuccessfully" do
+      before do
+        category.should_receive(:save) { false }
+        post :create
+      end
+
+      it { should render_template(:new) }
+    end
+  end
 end
