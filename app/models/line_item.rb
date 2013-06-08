cd2b8ee363629844dfbfbe3f4efa1bf4e4f45dfc
Jared Shay <jared.shay@gmail.com>
2013-06-08 18:35:02 +1000

    spec delete_line_item. There is still a TODO as destroying the line item isn't spec'd properly

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,17 @@ class LineItem < ActiveRecord::Base
   attr_accessible :quantity
   belongs_to :product
   belongs_to :cart
+
+  def destroy_line_item
+    # TODO: There is a race condition here.
+    if quantity > 1
+      # TODO: figure out why self was required here? I am using it as an
+      #  argument, not inside a method or some other shit.
+      update_attributes!(quantity: self.quantity -= 1)
+    else
+      destroy
+    end
+  end
 end
+
+
--- spec/models/line_items_spec.rb
+++ spec/models/line_items_spec.rb
@@ -13,4 +13,27 @@ describe LineItem do
       line_item.quantity.should == 1
     end
   end
+
+  describe "#destroy_line_item" do
+    let(:line_item) { FactoryGirl.create(:line_item) }
+
+    context "the line item quantity is greater than 1" do
+      it "should decrement the line item quantity" do
+        line_item.quantity = 2
+        line_item.destroy_line_item
+        line_item.quantity.should == 1
+      end
+    end
+
+    context "the line item quantity is 1" do
+      it "should destroy the line_item" do
+        line_item.quantity = 1
+        line_item.destroy_line_item
+        #TODO: WHY WONT YOU DIE? destroy_line_item appears to return destroy
+        # without complaining. But line item doesn't go away. It also reports
+        # it never received destroy.
+        line_item.should_receive(:destroy)
+      end
+    end
+  end
 end
